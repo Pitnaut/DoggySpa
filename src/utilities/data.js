8e545d8c29b7dc36d7e9a875fef5e76bcf233caf
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-07-04 11:43:06 +0200

    addd

---
+++ src/utilities/data.js
@@ -0,0 +1,19 @@
+export const monthNames = {
+  0: 'january',
+  1: 'february',
+  2: 'march',
+  3: 'april',
+  4: 'may',
+  5: 'june',
+  6: 'july',
+  7: 'august',
+  8: 'september',
+  9: 'october',
+  10: 'november',
+  11: 'december'
+};
+
+export const sizes = ["S (less than 5kg)", "M (between 5kg and 15kg)", "L (between 15kg and 25kg)", "XL (more than 25kg)"];
+export const hours = ["9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
+
+
